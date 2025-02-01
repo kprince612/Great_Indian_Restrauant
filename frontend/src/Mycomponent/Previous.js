82550da31064c069181225a7ff50a3124d15428c
kprince612 <khandelwalprince612@gmail.com>
2025-02-02 01:29:54 +0530

    df

--- frontend/src/Mycomponent/Previous.js
+++ frontend/src/Mycomponent/Previous.js
@@ -56,6 +56,11 @@ export default function Previous() {
     }
   };
 
+  const playsound = () => {
+    const audio = new Audio ("./ding-101492.mp3");
+    audio.play ();
+  }
+
   const verifyOrderOTP = async () => {
     try {
       const response = await axios.post ("https://great-indian-restrauant-gray.vercel.app/verify-order-otp", {email, otp});
@@ -63,6 +68,7 @@ export default function Previous() {
         alert ('Email verification Completed');
         setOtpVerify (true);
         fetchOrders ();
+        playsound ();
       }
 
       else {
